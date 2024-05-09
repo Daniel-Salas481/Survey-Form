074c78d5d4cca25e864bf9278f5f0e541c883f42
Daniel-Salas481 <dsalas6@asu.edu>
2024-05-08 19:25:06 -0700

    Added text-to-speech functionality.

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,1 +1,31 @@
-print("testing")
+from google.cloud import speech
+
+
+client = speech.SpeechClient.from_service_account_file('key.json')
+
+#file name changing soon
+file_name = "xxxx.mp3"
+
+with open(file_name, 'rb') as f:
+    mp3_data = f.read()
+
+#Obtaining data from mp3 file
+audio_file = speech.RecognitionAudio(content=mp3_data)
+
+
+#Configuration of the audio text-to-speech
+config = speech.RecognitionConfig(
+
+    sample_rate_hertz=44100,
+    enable_automatic_punctuation = True,
+    language_code = 'en-US'
+
+)
+
+
+response = client.recognize(
+    config=config,
+    audio=audio_file
+)
+
+print(response)
